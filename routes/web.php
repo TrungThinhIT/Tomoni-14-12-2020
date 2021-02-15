6e2c1e070b2880470bf12690d8d6bb512fd23518
anhmv <anhcdcn98@gmail.com>
2021-02-15 12:31:59 +0700

    anhmv

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\AddressBook\addressBookController;
+use App\Http\Controllers\addressbook\addressBookController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\PasswordResetController;
 use App\Http\Controllers\Customers\DebtController;
@@ -106,7 +106,7 @@ Route::prefix('/')->middleware('auth')->group(function () {
             Route::get('/delete/{Id}', [BillController::class, 'deleteBillCode'])->name('deleteBillCode');
             Route::get('/by-uname/{uname}', [BillController::class, 'indexAllByUname'])->name('indexAllByUname');
             Route::get('/log/{codeorder}', [BillController::class, 'loadLog'])->name('loadLog');
-            Route::get('/{billcode}', [BillController::class, 'getBillById'])->name('getBillById');
+            Route::get('/{billcode}', [BillController::class, 'addressBookController'])->name('getBillById');
             Route::get('/export/{billcode}', [BillController::class, 'exportBillById'])->name('exportBillById');
             Route::get('/tranfer/{codeorder}', [BillController::class, 'getTranfer'])->name('getTranfer');
             Route::put('/tranfer/{codeorder}', [BillController::class, 'putTranfer'])->name('putTranfer');
